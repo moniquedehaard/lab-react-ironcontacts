cd3a35894bb77c336b55332807fe2e63b0dc248c
Monique de Haard <moniquedehaard@msn.com>
2020-12-02 20:19:33 +0100

    untill iteration 3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const firstFive = contacts.slice(0, 5);
+// console.log(firstFive);
+
+class App extends React.Component {
+  state = {
+    counter: 5,
+    displayed: contacts.slice(0, 5),
+  }
+
+  handleClick = () => {
+    const count = this.state.counter+1;
+    this.setState({
+      counter: count,
+      displayed: contacts.slice(0,count)
+    })
+  }  
+  
+  sortNameByClick = () => {
+    const currentList = contacts.slice(0, this.state.counter);
+    currentList.sort((a,b) => {
+      const name1 = a.name.toLowerCase();
+      const name2 = b.name.toLowerCase();
+
+      if (name1 < name2){
+        return -1
+      } else if (name1 > name2) {
+        return 1
+      } else {
+        return 0
+      }
+    })
+
+    this.setState({
+      displayed : currentList
+    })
+  }
+
+  sortPopByClick = () => {
+    const currentList = contacts.slice(0, this.state.counter);
+
+    currentList.sort((a,b) => {
+      return b.popularity - a.popularity
+    });
+
+    this.setState({
+      displayed: currentList
+    })
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>IronContacts</h1>
+        <button onClick={this.handleClick}>Click here for next actor</button>
+        <button onClick={this.sortNameByClick}> Sort by name </button>
+        <button onClick={this.sortPopByClick}> Sort by popularity </button>
+  
+        <table>
+          <thead>
+            <tr>
+              <th> Picture </th>
+              <th> Name </th>
+              <th> Popularity </th>
+            </tr>
+          </thead>
+          <tbody>         
+            {this.state.displayed.map(data => {
+              return (
+                <tr key={data.id} >
+                  <td> <img src={data.pictureUrl}  alt="actor"/> </td>
+                  <td> {data.name} </td>
+                  <td> {data.popularity} </td>
+                 </tr>
+              )
+            })
+          }
+          </tbody>
+         </table>
+      </div>
+    ) 
+  }
 }
 
 export default App;
